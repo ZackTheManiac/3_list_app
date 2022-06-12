d91fa6ae466439cb00154b0fa42a422c1d41f6b5
ZackTheManiac <zhu.yicheng@yahoo.com>
2022-06-12 17:11:16 +0800

    Added flatlist

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ScrollView, FlatList } from "react-native";
+import names from "./names";
+
+const namesAsObject = names.map((name) => {
+  return {
+    name: name,
+  };
+});
+
+// ["Carl", "Bob", "Alice"] ==> [{ name: "Carl" }, { name: "Bob" }, { name: "Alice"} ]
+// essentially what the map function in javascript does
 
 export default function App() {
+  const renderItem = ({ item }) => {
+    return (
+      <View style={styles.listItem}>
+        <Text>{item.name}</Text>
+      </View>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FlatList
+      data={namesAsObject}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.name}
+    />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  listItem: {
+    padding: 20,
+    margin: 10,
+    backgroundColor: "lightblue",
+    borderBottomColor: "#999",
+    borderBottomwwidth: 1,
+    shadowColor: "black",
+    shadowRadius: 5,
+    shadowOpacity: 0.2,
+    shadowOffset: { x: 0, height: 3 },
   },
 });
